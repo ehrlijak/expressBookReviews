7dba3275e20bdc407a17214465117b5ebbd43c76
Red <jakubehrlich6@gmail.com>
2025-11-27 10:02:46 -0500

    some changes

--- router/general.js
+++ router/general.js
@@ -10,21 +10,34 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
+
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ 
+ return res.status(200).send(JSON.stringify(books, null, 2));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;        // 1. Get ISBN from URL
+    const book = books[isbn];            // 2. Lookup book in database
+
+    if (book) {                          // 3. If found
+        return res.json(book);           // Respond with JSON
+    } else {                             // 4. If not found
+        return res.status(404).json({ message: "Book not found" });
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+  const auth = req.params.author;
+
+  for ( book at books ) {
+
+	if (book.author == /////
+
+}
+
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
