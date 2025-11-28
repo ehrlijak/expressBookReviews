e1192167e192ab0079d8e767cca92bbe14c0b3b8
ehrlijak <jakubehrlich6@gmail.com>
2025-11-28 08:04:05 +0100

    Update general.js
    yes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,29 +10,56 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
+
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ 
+ return res.status(200).send(JSON.stringify(books, null, 2));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;        // 1. Get ISBN from URL
+    const book = books[isbn];            // 2. Lookup book in database
+
+    if (book) {                          // 3. If found
+        return res.json(book);           // Respond with JSON
+    } else {                             // 4. If not found
+        return res.status(404).json({ message: "Book not found" });
+    }
+});
   
-// Get book details based on author
+
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const keys = Object.keys(books);
+let matches = [];
+
+for (let key of keys) {
+    let book = books[key];
+    if (book.author === req.params.author) {
+        matches.push(book);
+    }
+}
+
+return res.json(matches);
+ 
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+ 
+	const keys = Object.keys(books);
+
+	let matches = [];
+
+	for (let key of keys) {
+    let book = books[key];
+    if (book.title === req.params.title) {
+        matches.push(book);
+    }
+}
+
+return res.json(matches);		
+
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
